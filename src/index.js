40a808ce92727ba11dac9d4ecbee4b6d18090e5e
Leoci Melo <leocij@hotmail.com>
2023-04-20 18:18:17 -0300

    feature-18-design-users-list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+
+import 'bootswatch/dist/minty/bootstrap.min.css'
+
 import Pages from './Pages';
 
-ReactDOM.render(<Pages />,document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Pages />
+)
