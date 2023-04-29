34a05cdc7df0e9ccd6529354b832a29e87b48bb7
Leoci Melo <leocij@hotmail.com>
2023-04-29 12:34:16 -0300

    feature-19-navbar

--- src/Pages.js
+++ src/Pages.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 // https://reactrouter.com/en/v6.3.0/getting-started/overview
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; //yarn add react-router-dom
-import UserList from './pages/UserList';
+import Navbar from './config/navbar/Navbar';
+import Home from './pages/Home';
+import UserList from './pages/users/UserList';
 
 export default function Pages() {
     return (
         <BrowserRouter>
+            <Navbar />
             <Routes>
+                <Route exact path="/home" element={<Home />} />
                 <Route exact path="/user-list" element={<UserList />} />
             </Routes>
         </BrowserRouter>
---
+++ src/config/navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleToggle = () => {
+        if (isOpen) {
+            setIsOpen(!isOpen);
+        }
+    }
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbar01"
+                aria-controls="navbars01"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <span className="text-white">Menu</span>
+            </button>
+            <div className={`${isOpen ? 'show' : ''} collapse navbar-collapse`} id="navbar01">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <NavLink to="/home" className="nav-link" onClick={handleToggle}>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/user-list" className="nav-link" onClick={handleToggle}>Users</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
