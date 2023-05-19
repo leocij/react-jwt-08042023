99c029f0eea3001d28ad0f2c6161328d025ae388
Leoci Melo <leocij@hotmail.com>
2023-05-19 19:03:55 -0300

    feature-22-credential-post

---
+++ src/main/java/com/empresa/postgresqlspringbootjwt05032023/models/Credential.java
@@ -0,0 +1,51 @@
+package com.empresa.postgresqlspringbootjwt05032023.models;
+
+import java.util.Date;
+
+public class Credential {
+    private int id;
+    private String email;
+    private String password;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return this.updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
