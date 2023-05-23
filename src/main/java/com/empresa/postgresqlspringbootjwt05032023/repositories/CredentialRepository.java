a7b51814c365604f93e8b42276d3e9be3a49abbc
Leoci Melo <leocij@hotmail.com>
2023-05-23 17:11:51 -0300

    feature-25-get-credentials

--- src/main/java/com/empresa/postgresqlspringbootjwt05032023/repositories/CredentialRepository.java
+++ src/main/java/com/empresa/postgresqlspringbootjwt05032023/repositories/CredentialRepository.java
@@ -12,6 +12,34 @@ import com.empresa.postgresqlspringbootjwt05032023.models.Credential;
 
 public class CredentialRepository {
 
+    public List<Credential> findAll() {
+        List<Credential> credentials = new ArrayList<Credential>();
+
+        try {
+            String url = "jdbc:postgresql://localhost:5432/postgresql_springboot_jwt_05032023_db";
+            String myUser = "postgres";
+            String myPassword = "postgres";
+            String sql_query = "select * from credentials;";
+            Connection connection = DriverManager.getConnection(url, myUser, myPassword);
+            PreparedStatement preparedStatement = connection.prepareStatement(sql_query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Credential credential = new Credential();
+                credential.setId(resultSet.getInt("id"));
+                credential.setEmail(resultSet.getString("email"));
+                credential.setPassword(resultSet.getString("password"));
+                credential.setCreatedAt(resultSet.getTimestamp("created_at"));
+                credential.setUpdatedAt(resultSet.getTimestamp("updated_at"));
+                credentials.add(credential);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
+
+        return credentials;
+    }
+
     public void save(Credential credential) {
         try {
             String url = "jdbc:postgresql://localhost:5432/postgresql_springboot_jwt_05032023_db";
