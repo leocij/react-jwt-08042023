8ea6ca16fe914db2513300bceabdf83bade20b0b
Leoci Melo <leocij@hotmail.com>
2023-06-01 17:00:29 -0300

    feature-31-auth-users

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,10 +1,16 @@
 import apiAxios from '../config/ApiAxios';
 
+const headers = {
+    headers: {
+        authorization: `Bearer ${localStorage.getItem('auth-token')}`
+    }
+}
+
 export const index = async () => {
-    const response = await apiAxios.get('/users');
+    const response = await apiAxios.get('/users', headers);
     return response.data;
 }
 
 export const postUser = async ({name}) => {
-    await apiAxios.post("/users", {name});
+    await apiAxios.post("/users", {name}, headers);
 }
